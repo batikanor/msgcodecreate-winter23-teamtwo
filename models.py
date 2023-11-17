f1850cc626b696ecba5e76a92d513192835e76f0
BATIKAN BORA ORMANCI <batikanor@gmail.com>
2023-11-18 00:21:05 +0100

    Add descriptive comments for user model

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
+# SQLAlchemy is our ORM (Object-Relational Mapping) tool.
 db = SQLAlchemy()
 
 bcrypt = Bcrypt()
@@ -10,6 +11,7 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
     def set_password(self, password):
+        # I believe bcrypt also salts it. See documentation for further info, but I think this is fine.
         self.password = bcrypt.generate_password_hash(password).decode('utf-8')
 
     def check_password(self, password):
