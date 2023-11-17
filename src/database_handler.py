057baad9f46b283bc97993ba6bc0b3caf262e487
Firas Mahmalat <Firas.mahmalat@rwth-aachen.de>
2023-11-17 19:48:15 +0100

    singleton class for database handler

--- src/database_handler.py
+++ src/database_handler.py
@@ -1,8 +1,11 @@
 import sqlite3
 
 
-class database_handler:
+from src.singleton import Singleton
 
+
+class database_handler(object):
+    __metaclass__ = Singleton
     conn = None
 
     def __init__(self) -> None:
@@ -11,3 +14,23 @@ class database_handler:
     def get_table(table_name):
         pass
 
+    def execute(self, query, data):
+        self.cur.execute(query, data)
+        self.conn.commit()
+        return self.cur
+
+    def add_user(self, user_name, team_name):
+        """ Creates a new orphan user in system with no league id assigned. """
+        return self.execute("INSERT INTO users (user_name, team_name VALUES (?, ?)", (user_name, team_name))
+
+    def assign_user_to_league(self, user_id, league_id):
+        """ Assigns a league_id to a user which is a foreign key """
+        return self.execute("UPDATE users SET league_id = ? WHERE user_id = ?", (league_id, user_id))
+
+    def add_player_to_game(self, player_name, team):
+        """ This function is only called  during initial set up of game, populates the player db with players. """
+        return self.execute("INSERT INTO players (name, team) VALUES(?, ?)", (player_name, team))
+
+    def _del_(self):
+        """ Destroys instance and connection on completion of called method """
+        self.conn.close()
