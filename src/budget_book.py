df04756d8de6b5cda2a7b4dbbfce21faaca6faf7
Firas Mahmalat <Firas.mahmalat@rwth-aachen.de>
2023-11-17 19:23:35 +0100

    fixed bug

--- src/budget_book.py
+++ src/budget_book.py
@@ -6,6 +6,5 @@ class budget_book:
     
     db_connection: sqlite3
 
-    def __init__(self, money_amount_: float, categorie_: str, comment_: str = "", date_of_transaction: datetime = N) -> None:
+    def __init__(self) -> None:
         self.db_connection = sqlite3.connect('budget_book.db')
-        
