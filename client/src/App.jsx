d831a86e43f2db1348a5dabdda9195ece5bc2071
BATIKAN BORA ORMANCI <batikanor@gmail.com>
2023-11-17 23:04:04 +0100

    Introduce sample backend connection to the frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/msg.svg'
 import viteLogo from './assets/msg.svg'
 import axios from 'axios';
@@ -7,6 +7,19 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   const [backendResponse, setBackendResponse] = useState('');
+  
+  // hook for side effect in component to make a sample request.
+  useEffect(() => {
+    // sample Axios GET request to the Flask server
+    console.log('Making request to Flask backend');
+    axios.get('http://127.0.0.1:5000/')
+      .then(response => {
+        setBackendResponse(response.data.status);
+      })
+      .catch(error => {
+        console.error('There was an error!', error);
+      });
+  }, []); // empty array here means this effect runs 1 time after the initial render
 
   return (
     <>
@@ -30,6 +43,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+
+      <p>Sample Backend Response: {backendResponse}</p>
     </>
   )
 }
