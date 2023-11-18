ec6a0d5a09527e5799d69a73d6dd47a8ec13edf0
Erkan Turan <erkantare07@gmail.com>
2023-11-18 21:01:08 +0100

    push logout button all the way to the right

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -14,10 +14,9 @@ function IndexPage() {
 
   return (
     <>
-  <h1 className="text-4xl font-bold text-center">Budget Plan</h1>
-
-        <button className="bg-red-500 p-1 text-white rounded" onClick={handleLogout}>Logout</button>
-      <BudgetBookTable />
+        <button className="fixed right-0 bg-red-500 text-white px-4 py-2" onClick={handleLogout}>Logout</button>
+        <h1 className="text-4xl font-bold text-center">Budget Plan</h1>
+        <BudgetBookTable />
     </>
   );
 }
